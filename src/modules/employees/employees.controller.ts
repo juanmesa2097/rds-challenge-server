1e8b37485dbb03c0fca03288d3096f0b16007e14
juanmesa2097 <juanmesa2097@gmail.com>
2021-05-12 15:13:27 -0500

    feat: create employees services

--- src/modules/employees/employees.controller.ts
+++ src/modules/employees/employees.controller.ts
@@ -1,12 +1,53 @@
-import { Controller } from '@nestjs/common';
+import { WrapperResult } from '@common/interfaces';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { AddEmployeeDto } from './dtos/add-employee.dto';
+import { GetEmployeeDto } from './dtos/get-employee.dto';
 import { EmployeesService } from './employees.service';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private employeesService: EmployeesService) {}
 
-  // @Get()
-  // async getAll(): Promise<> {
+  @Get()
+  async getAll(): Promise<WrapperResult<GetEmployeeDto[]>> {
+    return await this.employeesService.getAll();
+  }
 
-  // }
+  @Get(':id')
+  async getById(
+    @Param('id', new ParseIntPipe()) id: number,
+  ): Promise<GetEmployeeDto> {
+    return await this.employeesService.getById(id);
+  }
+
+  @Post()
+  async create(
+    @Body() addEmployeeDto: AddEmployeeDto,
+  ): Promise<GetEmployeeDto> {
+    return await this.employeesService.create(addEmployeeDto);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', new ParseIntPipe()) id: number,
+    @Body() addEmployeeDto: AddEmployeeDto,
+  ): Promise<GetEmployeeDto> {
+    return await this.employeesService.update(id, addEmployeeDto);
+  }
+
+  @Delete(':id')
+  async delete(
+    @Param('id', new ParseIntPipe()) id: number,
+  ): Promise<GetEmployeeDto> {
+    return await this.employeesService.delete(id);
+  }
 }
--- src/modules/employees/employees.service.ts
+++ src/modules/employees/employees.service.ts
@@ -1,4 +1,76 @@
-import { Injectable } from '@nestjs/common';
+import { WrapperResult } from '@common/interfaces';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { classToPlain, plainToClass } from 'class-transformer';
+import { AddEmployeeDto } from './dtos/add-employee.dto';
+import { GetEmployeeDto } from './dtos/get-employee.dto';
+import { UpdateEmployeeDto } from './dtos/update-employee.dto';
+import { EmployeesRepository } from './employees.repository';
 
 @Injectable()
-export class EmployeesService {}
+export class EmployeesService {
+  constructor(private readonly employeesRepository: EmployeesRepository) {}
+
+  async getAll(): Promise<WrapperResult<GetEmployeeDto[]>> {
+    try {
+      const [result, total] = await this.employeesRepository.findAndCount();
+
+      return {
+        data: result.map((employee) => plainToClass(GetEmployeeDto, employee)),
+        meta: { count: total },
+      };
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async getById(id: number): Promise<GetEmployeeDto> {
+    try {
+      const employee = await this.employeesRepository.findOne(id);
+
+      if (!employee) {
+        throw new NotFoundException(`Employee not found`);
+      }
+
+      return plainToClass(GetEmployeeDto, employee);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  async create(addEmployeeDto: AddEmployeeDto): Promise<GetEmployeeDto> {
+    const employeeToSave = classToPlain(addEmployeeDto);
+    const newEmployee = this.employeesRepository.create(employeeToSave);
+    await this.employeesRepository.save(newEmployee);
+    return plainToClass(GetEmployeeDto, newEmployee);
+  }
+
+  async update(
+    id: number,
+    updateEmployeeDto: UpdateEmployeeDto,
+  ): Promise<GetEmployeeDto> {
+    const employee = await this.employeesRepository.findOne(id);
+
+    if (!employee) {
+      throw new NotFoundException(`Employee not found`);
+    }
+
+    const employeeToUpdate = classToPlain(updateEmployeeDto);
+    await this.employeesRepository.save(employeeToUpdate);
+    return plainToClass(GetEmployeeDto, employee);
+  }
+
+  async delete(id: number): Promise<GetEmployeeDto> {
+    const employee = await this.employeesRepository.findOne(id);
+
+    if (!employee) {
+      throw new NotFoundException(`Employee not found`);
+    }
+
+    await this.employeesRepository.delete(id);
+    return plainToClass(GetEmployeeDto, employee);
+  }
+}
